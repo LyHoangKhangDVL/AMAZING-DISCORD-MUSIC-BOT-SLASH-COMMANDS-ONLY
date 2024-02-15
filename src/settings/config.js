10bef6a66b9e8bce08a6c98b99cc7b75700bf4e4
LyHoangKhangDVL <121933718+LyHoangKhangDVL@users.noreply.github.com>
2024-02-15 20:02:58 +0700

    Update config.js

--- src/settings/config.js
+++ src/settings/config.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 
 module.exports = {
 
-  token: process.env.TOKEN || "", // <==== PASTE YOU TOKEN
+  token: process.env.TOKEN || "MTIwNzY3MjgzMTIxNTY2OTMxOA.GUg-kB.7mY7I8rmbcC5YSQD-15F6vWrJWsnSdDYFWXUzw", // <==== PASTE YOU TOKEN
   prefix: process.env.PREFIX || ".", // <==== SET YOU PRERIX BOT [ OWNER COMMANDS ]
   color: process.env.EMBED_COLOR || "#2f3136", // <==== YOU EMBEDED HEX COLOR
-  owner: process.env.OWNER_ID || "1173547185758015498", // <==== BOTS OWNER ID
-  guildLogs: process.env.GUILD_LOGS || "1198586029343518820", // <==== YOUR SERVER JOIN LEFT LOGS CHANNEL ID
-  leaveTimeout: process.env.LEAVE_TIMEOUT || "20000", // <==== SET LEAVE TIMEOUT WHEN BOT WAS ALONE || 1000 = 1sec
+  owner: process.env.OWNER_ID || "1053871662283628615", // <==== BOTS OWNER ID
+  guildLogs: process.env.GUILD_LOGS || "1204057432112889926", // <==== YOUR SERVER JOIN LEFT LOGS CHANNEL ID
+  leaveTimeout: process.env.LEAVE_TIMEOUT || "1000", // <==== SET LEAVE TIMEOUT WHEN BOT WAS ALONE || 1000 = 1sec
   disableYouTube: parseBoolean(process.env.DISABLE_YOUTUBE || "false"), // <==== SET "TRUE OR FALSE" | ENABLE/DISABLE YOUTUBE FEATURES. DISABLING THIS WILL MAKE "AUTOPLAY" COMMANDS USELESS!!!
 
   // ⬇⬇⬇ PORU DETAILS
@@ -17,9 +17,9 @@ module.exports = {
     clientID: process.env.SPOTIFY_ID || "e6f84fbec2b44a77bf35a20c5ffa54b8", // <==== SPOTIFY CLIENT ID
     clientSecret: process.env.SPOTIFY_SECRET || "498f461b962443cfaf9539c610e2ea81", // <==== SPOTIFY CLIENT SECRET
     reconnectTries: 5, // <==== TOTAL ATTEMPS TO TRY IF RECONNECT FAILED. YOU CAN CHANGE IT TO "Infinity" FOR UNLIMITED ATTEMPS.
-    playlistLimit: 2, // <==== 1 = 100 TRACKS
-    albumLimit: 2, // <==== 1 = 50 TRACKS
-    artistLimit: 2, // <==== 1 = 50 TRACKS
+    playlistLimit: 5, // <==== 1 = 100 TRACKS
+    albumLimit: 5, // <==== 1 = 50 TRACKS
+    artistLimit: 5, // <==== 1 = 50 TRACKS
     searchMarket: "us",
   },
   nodes: [
@@ -35,10 +35,10 @@ module.exports = {
 
 
   mongoUri: process.env.MONGO_URI || "mongodb+srv://pee33:wnn99e@cluster0.fgivnid.mongodb.net/?retryWrites=true&w=majority", 
-  supportUrl: process.env.SUPPORT_URL || "https://discord.gg/7MEHnM6T87", 
-  voteUrl: process.env.VOTE_URL || "https://discord.gg/7MEHnM6T87",
-  inviteUrl: process.env.INVITE_URL || "https://discord.com/api/oauth2/authorize?client_id=1197585882803343391&permissions=40271764260928&scope=bot%20applications.commands", 
-  imageUrl: process.env.IMAGE_URL || "https://media.discordapp.net/attachments/1162795987014787162/1197198433962971156/pngtree-pair-of-headphones-on-the-water-at-nighttime-image_2931863.png?ex=65c39f6d&is=65b12a6d&hm=e94858f4cbfc92ee4868da52953a4015b2c2703c5f0e12f2e144d446be72d8fe&=&format=webp&quality=lossless&width=800&height=448",
+  supportUrl: process.env.SUPPORT_URL || "https://discord.gg/wZHjUG4AYC", 
+  voteUrl: process.env.VOTE_URL || "https://discord.gg/wZHjUG4AYC",
+  inviteUrl: process.env.INVITE_URL || "https://discord.com/api/oauth2/authorize?client_id=1207672831215669318&permissions=8&scope=bot", 
+  imageUrl: process.env.IMAGE_URL || "https://media.discordapp.net/attachments/1204018606980071424/1207671823752503336/55e2c17d24b5888415c2a93c0b265c92.gif?ex=65e07f08&is=65ce0a08&hm=b22e02666f061bd708b6bc0cc9a53ebd2ebf46343cfd23b0d29ff25e99ff518d&",
 };
 
 function parseBoolean(value) {
